ea72a1e5bf87e2b0c6730d31d273bbb9b8a7b4cf
tebr1z <tabrizyh@code.edu.az>
2023-06-11 23:15:01 +0400

    Eror fixed

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -4,7 +4,12 @@ import { RiShoppingCart2Fill } from "react-icons/ri";
 
 const MenuItem = ({ product }) => {
   return (
- 
+    <div className="bg-secondary rounded-3xl">
+      <div className="w-full  bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
+      <Link href={`/product/${product._id}`}>
+          <div className="relative w-36 h-36 hover:scale-110 transition-all">
+            <Image src={product.img} alt="" layout="fill" priority />
+          </div>
         </Link>
       </div>
       <div className="p-[25px] text-white">
