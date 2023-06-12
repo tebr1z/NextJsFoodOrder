3ddfabfb8a4f98cbe5399c16eff93c12cbbdefda
tebr1z <tabrizyh@code.edu.az>
2023-06-12 23:16:45 +0400

    Wrapper menu eror fixed

--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -18,8 +18,26 @@ const MenuWrapper = ({ categoryList, productList }) => {
 
   return (
     <div className="container mx-auto  mb-16">
-    
-    
+      <div className="flex flex-col items-center w-full">
+        <Title addClass="text-[40px]">Our Menu</Title>
+        <div className="mt-10">
+          {categoryList &&
+            categoryList.map((category, index) => (
+              <button
+                className={`px-6 py-2  rounded-3xl ${
+                  index === active && "bg-secondary text-white"
+                }`}
+                key={category._id}
+                onClick={() => {
+                  setActive(index);
+                  setProductLimit(3);
+                }}
+              >
+                {category.title}
+              </button>
+            ))}
+        </div>
+      </div>
       <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 min-h-[450px]">
         {filter.length > 0 &&
           filter
