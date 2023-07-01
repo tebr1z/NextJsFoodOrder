52bb13d9bba9a9ebc88afaf6d2f93a159d76036f
tebr1z <tabrizyh@code.edu.az>
2023-07-02 00:16:52 +0400

    Home index

---
+++ pages/home/index.jsx
@@ -0,0 +1,26 @@
+
+import React from "react";
+import About from "../../components/About";
+import Campaigns from "../../components/Campaigns";
+import Carousel from "../../components/Carousel";
+import Customers from "../../components/customers/Customers";
+import MenuWrapper from "../../components/product/MenuWrapper";
+import Reservation from "../../components/Reservation";
+import ChatWidget from "@/components/comment/ChatWidget";
+
+const Index = ({ categoryList, productList }) => {
+  return (
+    <React.Fragment>
+      <Carousel />
+      <Campaigns />
+      <MenuWrapper categoryList={categoryList} productList={productList} />
+      <About />
+      <Reservation />
+      <Customers />
+      <ChatWidget />
+    </React.Fragment>
+  );
+};
+
+
+export default Index;
